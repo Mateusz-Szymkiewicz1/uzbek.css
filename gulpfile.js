00ebd50082c9f8d10a73dc41b8c5560b17238e2c
unknown <szymkiewiczmateusz1@gmail.com>
2023-07-07 23:21:20 +0200

    big file

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 const { src, dest, watch, series } = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const purgecss = require('gulp-purgecss')
+const rename = require('gulp-rename')
 
 function buildStyles() {
+  src('uzbek/**/*.scss').pipe(sass({ outputStyle: 'compressed' })).pipe(rename('uzbek.css')).pipe(dest('css'));
   return src('uzbek/**/*.scss')
     .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(purgecss({ content: ['*.html'] }))
