de8fcc214542d0bc72f10d7838c2d52f862bc4a4
unknown <szymkiewiczmateusz1@gmail.com>
2023-07-11 23:26:23 +0200

    landing page but even more

--- index.js
+++ index.js
@@ -1,55 +1,7 @@
-if(localStorage.getItem('uzbek_theme')){
-  if(localStorage.getItem('uzbek_theme') == 'dark'){
-    document.querySelector('.fa-sun').className = 'fa fa-moon';
-    document.body.classList.add('dark');
-  }
-}
-
-function preCode(selector) {
-  const els = Array.prototype.slice.call(document.querySelectorAll(selector), 0);
-  els.forEach(function(el) {
-    const txt = el.textContent.replace(/^[\r\n]+/, "").replace(/\s+$/g, "");
-    if (/^\S/gm.test(txt)) {
-      el.textContent = txt;
-      return;
-    }
-    let mat, str, re = /^[\t ]+/gm, len, min = 1e3;
-    while (mat = re.exec(txt)) {
-      len = mat[0].length;
-      if (len < min) {
-		min = len;
-		str = mat[0];
-      }
-    }
-    if (min == 1e3)
-      return;
-      el.textContent = txt.replace(new RegExp("^" + str, 'gm'), "");
-    });
-}
-
-document.querySelector(".navbar i").addEventListener('click', function(e){
-  if(e.target.className == 'fa fa-sun'){
-    localStorage.setItem('uzbek_theme', 'dark');
-    e.target.className = 'fa fa-moon';
-    document.body.classList.add('dark');
+document.querySelector('.menu_icon').addEventListener('click', function(){
+  if(document.querySelector('.menu').style.display == 'block'){
+    document.querySelector('.menu').style.display = 'none';
   }else{
-    localStorage.setItem('uzbek_theme', 'light');
-    e.target.className = 'fa fa-sun';
-    document.body.classList.remove('dark');
+    document.querySelector('.menu').style.display = 'block';
   }
 })
-
-function toggle_code(code){
-  if(document.querySelector(`.${code}_target`).innerText){
-    event.target.innerText = 'show code';
-    document.querySelector(`.${code}_target`).parentElement.style.display = 'none';
-    document.querySelector(`.${code}_target`).innerText = '';
-  }else{
-    event.target.innerText = 'hide code';
-    document.querySelector(`.${code}_target`).parentElement.style.display = 'block';
-    let content = document.querySelector(`.${code}`).innerHTML;
-    document.querySelector(`.${code}_target`).textContent = content;
-    preCode(`.${code}_target`);
-    Prism.highlightElement(document.querySelector(`.${code}_target`));
-  }
-}
